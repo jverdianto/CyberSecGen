f9ae7089ec73929e268f5d75d5d3247fa9bd66f5
jverdianto <jeffreyverdianto55@gmail.com>
2023-09-29 16:23:48 +0700

    merapikan coding

--- script.js
+++ script.js
@@ -10,14 +10,6 @@ function addUserMessage(message) {
     chatLog.appendChild(userMessage);
     adjustMessageWidth(userMessage); // Menyesuaikan lebar balon pesan
     disableUserInput(); // Nonaktifkan input pengguna setelah mengirim pesan
-    // Lakukan pengolahan pesan chatbot di sini dan tambahkan respons chatbot
-    // Contoh sederhana:
-    setTimeout(function() {
-        const botResponse = "Ini respons dari chatbot.";
-        addBotMessage(botResponse);
-        isBotResponding = true; // Setelah bot merespons, pengguna dapat mengirim pesan lagi
-        enableUserInput(); // Aktifkan kembali input pengguna setelah bot merespons
-    }, 1000); // Contoh respons setelah 1 detik
 }
 
 function addBotMessage(message) {
@@ -37,15 +29,19 @@ function addBotMessage(message) {
             clearInterval(typingInterval); // Hentikan animasi ketika semua karakter ditampilkan
             adjustMessageWidth(botMessage);
         }
-    }, 50); // Interval waktu antara penambahan karakter
+    }, 20); // Interval waktu antara penambahan karakter
 }
 
 function adjustMessageWidth(messageElement) {
     const messageText = messageElement.textContent;
     const messageLength = messageText.length;
-    const maxWidth = Math.min(messageLength, 70); // Maksimum 70% lebar, disesuaikan dengan faktor 5
+    const maxWidth = Math.min(messageLength * messageLength + 32, 1000) + "px";
 
-    messageElement.style.maxWidth = maxWidth + "%";
+    if (maxWidth == 1000 + "px") {
+        maxWidth = 70 + "%"
+    }
+
+    messageElement.style.width = maxWidth;
 }
 
 function disableUserInput() {
@@ -58,50 +54,34 @@ function enableUserInput() {
     sendButton.disabled = false;
 }
 
-// sendButton.addEventListener("click", function() {
-//     if (!isBotResponding) {
-//         return; // Pengguna tidak dapat mengirim pesan sampai bot merespons
-//     }
-    
-//     const userMessage = userInput.value.trim();
-//     if (userMessage) {
-//         addUserMessage(userMessage);
+function sendUserMessage(userMessage) {
+    // Kirim pesan pengguna ke API
+    var raw = JSON.stringify({
+        "message": userMessage
+    });
 
-//         // Persiapkan header dan body permintaan
-//         const apiUrl = "http://192.168.193.109:5000/api"; // Ganti dengan URL API Anda
-//         const headers = {
-//             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-//             'Accept-Language': 'en-US,en;q=0.5',
-//             'Accept-Encoding': 'gzip, deflate',
-//             'Connection': 'close',
-//             'Upgrade-Insecure-Requests': '1',
-//             'Content-Type': 'application/json',
-//             'Content-Length': userMessage.length.toString()
-//         };
-//         const requestOptions = {
-//             method: 'POST',
-//             headers: headers,
-//             body: JSON.stringify({ message: userMessage })
-//         };
+    var requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: raw,
+        redirect: 'follow'
+    };
 
-//         // Kirim permintaan ke API
-//         fetch(apiUrl, requestOptions)
-//             .then(response => response.json())
-//             .then(data => {
-//                 // Tanggapan dari API
-//                 addBotMessage(data.botResponse); // Menambahkan respons chatbot ke log
-//                 isBotResponding = true;
-//                 enableUserInput();
-//             })
-//             .catch(error => {
-//                 console.error('Error:', error);
-//                 isBotResponding = true;
-//                 enableUserInput();
-//             });
-
-//         userInput.value = ""; // Hapus input pengguna
-//     }
-// });
+    fetch("http://192.168.193.109:5000/api", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result.message);
+        addBotMessage(result.message);
+        isBotResponding = true;
+        enableUserInput();
+    })
+    .catch(error => {
+        console.log('error', error);
+    });
+}
 
 sendButton.addEventListener("click", function(){
     if (!isBotResponding) {
@@ -111,28 +91,8 @@ sendButton.addEventListener("click", function(){
     const userMessage = userInput.value.trim();
     if (userMessage) {
         addUserMessage(userMessage);
-        
-        // Kirim pesan pengguna ke API
-        var raw = JSON.stringify({
-            "message": "What is cyber security?"
-        });
-
-        var requestOptions = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: raw,
-            redirect: 'follow'
-        };
-
-        fetch("http://192.168.193.109:5000/api", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-
-        userInput.value= "";
+        sendUserMessage(userMessage);
+        userInput.value = "";
     }    
 });
 
