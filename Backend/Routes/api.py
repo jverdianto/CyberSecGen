72d4646660e2705cf0ed18a4927283c5cd0d3ef7
jverdianto <jeffreyverdianto55@gmail.com>
2023-10-05 13:39:26 +0700

    Update api.py

--- Backend/Routes/api.py
+++ Backend/Routes/api.py
@@ -7,29 +7,29 @@ first_blueprint = Blueprint('first_blueprint', __name__, url_prefix="/api")
 def getMessage():
     if request.json:
         chatRequest = request.get_json()['message']
-        # chatKeyword = generateMessageKeyword(chatRequest)
-        # isValidKeyword = checkKeyword(chatKeyword)
-        # if isValidKeyword == True:
-        #     chatResponse = generateMessage(chatRequest)
-        #     data = {
-        #     'message': chatResponse,
-        #     'keyword': chatKeyword,
-        #     }
-        # else:
-        chatVerify = verifyMessage(chatRequest)
-        if chatVerify and ("yes" in chatVerify.lower() or "yes." in chatVerify.lower()):
+        chatKeyword = generateMessageKeyword(chatRequest)
+        isValidKeyword = checkKeyword(chatKeyword)
+        if isValidKeyword == True:
             chatResponse = generateMessage(chatRequest)
             data = {
-                'message': chatResponse,
-                # 'keyword': chatKeyword,
-                'verify': chatVerify
+            'message': chatResponse,
+            'keyword': chatKeyword,
             }
         else:
-            data = {
-                'message': "I'm sorry, I cannot assist you with that.",
-                # 'keyword': chatKeyword,
+            chatVerify = verifyMessage(chatRequest)
+            if chatVerify and ("yes" in chatVerify.lower() or "yes." in chatVerify.lower()):
+                chatResponse = generateMessage(chatRequest)
+                data = {
+                'message': chatResponse,
+                'keyword': chatKeyword,
                 'verify': chatVerify
-            }
+                }
+            else:
+                data = {
+                'message': "I'm sorry, I cannot assist you with that.",
+                'keyword': chatKeyword,
+                'verify': chatVerify
+                }
         return jsonify(data)
     else:
         return jsonify({'message': 'No JSON data found in the request'})
