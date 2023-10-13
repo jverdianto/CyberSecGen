aa990a72c45ad361611a0eeff1f1ba53a4f18a6d
Joshevan <joshevan@ui.ac.id>
2023-10-13 16:51:16 +0700

    Temporary chat completion

--- Backend/Routes/api.py
+++ Backend/Routes/api.py
@@ -3,72 +3,36 @@ from Backend.Routes.checkMessage import generateMessageKeyword, checkKeyword, ve
 import g4f
 
 app = Flask(__name__)
+messagesPrompt=[{"role": "system", "content": "You are a cyber security assistant. As a cyber security assistant, you can only answer things that are related to cyber security, you cannot answer things that are not related to cyber security even if you know that."}]
 
 first_blueprint = Blueprint('first_blueprint', __name__, url_prefix="/api")
-@first_blueprint.route('/data', methods=['POST'])
-def getMessage():
-    if request.json:
-        chatRequest = request.get_json()['message']
-        chatKeyword = generateMessageKeyword(chatRequest)
-        isValidKeyword = checkKeyword(chatKeyword)
-        if isValidKeyword == True:
-            chatResponse = generateMessage(chatRequest)
-            data = {
-            'message': chatResponse,
-            'keyword': chatKeyword,
-            }
-        else:
-            chatVerify = verifyMessage(chatRequest)
-            if chatVerify and ("yes" in chatVerify.lower() or "yes." in chatVerify.lower()):
-                chatResponse = generateMessage(chatRequest)
-                data = {
-                'message': chatResponse,
-                'keyword': chatKeyword,
-                'verify': chatVerify
-                }
-            else:
-                data = {
-                'message': "I'm sorry, I cannot assist you with that.",
-                'keyword': chatKeyword,
-                'verify': chatVerify
-                }
-        return jsonify(data)
-    else:
-        return jsonify({'message': 'No JSON data found in the request'})
-
 @first_blueprint.route('/streamdata', methods=['POST'])
 def getStreamMessage():
+    global messagesPrompt
     if request.json:
         chatRequest = request.get_json()['message']
-        chatKeyword = generateMessageKeyword(chatRequest)
-        isValidKeyword = checkKeyword(chatKeyword)
-        if isValidKeyword == True:
-            chatResponse = generateMessage(chatRequest, True)
-            def streaming():
-                for word in chatResponse:
-                    yield f'{word}'
-            return app.response_class(streaming(), mimetype='text/event-stream')
-        else:
-            chatVerify = verifyMessage(chatRequest)
-            if chatVerify and ("yes" in chatVerify.lower() or "yes." in chatVerify.lower()):
-                chatResponse = generateMessage(chatRequest, True)
-                def streaming():
-                    for word in chatResponse:
-                        yield f'{word}'
-                return app.response_class(streaming(), mimetype='text/event-stream')
-            else:
-                def streaming():
-                    response = "I'm sorry, I cannot assist you with that."
-                    for word in response:
-                        yield f'{word}'
-                return app.response_class(streaming(), mimetype='text/event-stream')
+        chatResponse = generateMessage(chatRequest, True)
+        def streaming():
+            tempMessage = ""
+            for word in chatResponse:
+                tempMessage = tempMessage + word
+                yield f'{word}'
+            messagesPrompt.append({"role": "assistant", "content": tempMessage})    
+        return app.response_class(streaming(), mimetype='text/event-stream')
     else:
         return jsonify({'message': 'No JSON data found in the request'})
 
-def generateMessage(chatRequest, stream = False):
+def generateMessage(chatRequest, stream = True):
+    global messagesPrompt
+    messagesPrompt.append({"role": "user", "content": chatRequest})
+    print(messagesPrompt)
     chatResponse = g4f.ChatCompletion.create(
         model=g4f.models.gpt_35_turbo_16k,
-        messages=[{"role": "user", "content": chatRequest}],
+        messages=messagesPrompt,    
         stream= stream
     )
     return chatResponse
+
+def appendMessages(messageResponse):
+    global messagesPrompt
+    messagesPrompt.append({"role": "user", "content": messageResponse})
